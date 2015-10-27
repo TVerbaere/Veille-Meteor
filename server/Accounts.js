3524513e50625b3c6ec63f3f87de75b3f862f5bc
thibaud <thibaud.verbaere@gmail.com>
2015-10-27 17:03:13 +0100

    modification dans les vérifications côté serveur pour les inscriptions -> erreurs en français

--- server/Accounts.js
+++ server/Accounts.js
@@ -1,17 +1,27 @@
-// Fonction de validation des données envoyées lors de l'inscription. Ne pas toucher !
+// Foncion de validation des données envoyées lors de l'inscription. Ne pas toucher !
 Accounts.validateNewUser( function(user) {
 
-  if (user.services.password.length < 5) {
-    throw new Meteor.Error(500,"Votre mot de passe doit contenir plus de 5 caractères.")
-  }
-
+  // Vérification 1 : l'adresse mail est sous une forme traditionnelle :
   if (/^[a-zA-Z][a-zA-Z0-9_\.\-]+@([a-zA-Z][a-zA-Z0-9-]+\.)+[a-zA-Z]{2,3}$/
-        .test(user.emails[0].address)
-      ) {
-    return true;
+        .test(user.emails[0].address)) {
+
   }
   else {
-    throw new Meteor.Error(500,"Veuillez donner une adresse email valide.");
+    throw new Meteor.Error(403,"Veuillez donner une adresse email valide.");
   }
+
+  // Vérification 2 : l'email entrée est déja utilisée :
+  var emailDejaExistante = Meteor.users.find({"emails.address": user.emails[0].address}, {limit: 1}).count() > 0;
+  if (emailDejaExistante) {
+    throw new Meteor.Error(403, "Votre email est déja utilisée par un autre membre.")
+  }
+
+  // Vérification 3 : le pseudo est deja utilisé :
+  var pseudoDejaExistant = Meteor.users.find({"username": user.username}, {limit: 1}).count() > 0;
+  if (pseudoDejaExistant) {
+    throw new Meteor.Error(403, "Le pseudo est déja utilisé par un autre membre.")
+  }
+
+  return true;
 
 });
