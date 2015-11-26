c5e48cd903c51228be831039ee04cbfce0358021
thibaud <thibaud.verbaere@gmail.com>
2015-11-26 18:04:27 +0100

    limitation retours

--- server/lib/publications.js
+++ server/lib/publications.js
@@ -1,11 +1,11 @@
 // Publication des messages de channel 'a' (a null pour le chat public) :
 Meteor.publish('messages', function(a) {
-  return Messages.find( {channel: a}, {sort : {heure : -1}});
+  return Messages.find( {channel: a} );
 });
 
 // Publication des channels :
 Meteor.publish('channels', function() {
-  return Channels.find( {utilisateurs: this.userId}, {sort : {heure : -1}});
+  return Channels.find( {utilisateurs: this.userId} );
 });
 
 // Publication des utilisateurs :
